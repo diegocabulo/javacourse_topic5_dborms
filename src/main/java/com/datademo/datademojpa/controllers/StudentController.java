db4f76c23bd954310d210ee3c7633818d822b476
dcabulo <diego.cabulo@linktic.co>
2022-03-03 12:07:27 -0500

    feat: get and post method student

--- src/main/java/com/datademo/datademojpa/controllers/StudentController.java
+++ src/main/java/com/datademo/datademojpa/controllers/StudentController.java
@@ -3,11 +3,13 @@ package com.datademo.datademojpa.controllers;
 import com.datademo.datademojpa.domain.Student;
 import com.datademo.datademojpa.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
@@ -20,7 +22,17 @@ public class StudentController {
     }
 
     @GetMapping
-    public List<Student> getStudents(){
-        return studentService.getStudents();
+    public ResponseEntity<List<Student>> getAllStudents(){
+        List<Student> students = studentService.getStudents();
+        return new ResponseEntity<>(students, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Map<String, Boolean>> registerNewUser(@RequestBody Student student){
+        studentService.addNewStudent(student);
+        Map<String, Boolean> map = new HashMap<>();
+        map.put("sucess", true);
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
+
     }
 }
--- src/main/java/com/datademo/datademojpa/repositories/StudentRepository.java
+++ src/main/java/com/datademo/datademojpa/repositories/StudentRepository.java
@@ -2,8 +2,14 @@ package com.datademo.datademojpa.repositories;
 
 import com.datademo.datademojpa.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    @Query("SELECT s FROM Student s where s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 }
